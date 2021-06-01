001cd2e0c69d542947acd8cc422e24f25f4340e6
Angela <angkohtenko@gmail.com>
2021-06-01 14:05:14 -0400

    summary suspension_coil calculated

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,17 @@ model <- lm(MechaCar$mpg ~ MechaCar$vehicle_length +
            MechaCar$AWD, MechaCar)
 
 summary(model)
+
+# Import and read suspension_coil dataset as a dataframe
+suspension_coil <- read.csv(file="Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI),
+                                               Median = median(PSI),
+                                               Variance = var(PSI),
+                                               SD = sd(PSI))
+total_summary
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),
+                                                                             Median = median(PSI),
+                                                                             Variance = var(PSI),
+                                                                             SD = sd(PSI))
+lot_summary
+
